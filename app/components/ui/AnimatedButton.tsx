0fba57d6ad68a8c637c6686ed55c48d9cf356faf
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-08-11 19:53:44 +0600

    fix: add link target to new tab

--- app/components/ui/AnimatedButton.tsx
+++ app/components/ui/AnimatedButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 function AnimatedButton({ link, text }: { link: string, text: string }) {
     return (
-        <Link className="animated-button bg-transparent font-base font-para text-accent px-6 py-4 border border-accent rounded inline-block mx-auto" href={link}>{text}</Link>
+        <Link className="animated-button bg-transparent font-base font-para text-accent px-6 py-4 border border-accent rounded inline-block mx-auto" href={link} target="_blank" >{text}</Link>
     )
 }
 
