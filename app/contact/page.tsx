40a5c55531bf92780f0608793813b0e1393ae91c
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-08-15 20:05:07 +0600

    feat: added glow effect on contact form of the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ function ContactPage() {
                     <h1 className='font-heading text-neutral-0 text-3xl font-extrabold text-center md:text-left md:text-[4rem] md:leading-[76px] md:font-bold'>Let&apos;s Connect</h1>
                     <p className='font-para text-base text-neutral-100 text-center mt-8 max-w-[600px] md:text-left md:text-[1.25rem] md:leading-[32px]'>If you’re interested in working together, have a project in mind, or just want to chat about web development, don’t hesitate to reach out. I’m always open to new opportunities and would love to hear from you!</p>
                 </div>
-                <div className='bg-primary p-6 rounded md:max-w-md md:w-full md:p-12 '>
+                <div className='contact-form bg-primary p-6 rounded md:max-w-md md:w-full md:p-12'>
                     <ContactForm />
                 </div>
 
