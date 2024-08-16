2745f1846cf09a0ad21fdf76e2e181b37a193061
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-08-16 20:34:46 +0600

    fix: Update Hero component with revised text and description

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,9 +5,9 @@ function Hero() {
         <section className="text-center my-20 md:mb-32 px-6 md:text-left md:mx-auto md:max-w-[896px] md:container md:px-6" id="hero">
             <span className="text-neutral-100 block mb-4 font-normal font-para md:mb-10">Hello, I am</span>
             <h1 className='text-neutral-0 text-3xl font-extrabold font-heading mb-4 md:text-[3.5rem] md:mb-8'>Sreenath Kumar</h1>
-            <h2 className='text-neutral-100 text-3xl mb-6 font-extrabold font-heading md:text-[3.5rem] md:mb-10'>I make Web Beautiful.</h2>
+            <h2 className='text-neutral-100 text-3xl mb-6 font-extrabold font-heading md:text-[3.5rem] md:mb-10'>Bringing Pixels to Life.</h2>
             <p className='px-6 mb-10 text-neutral-100 font-para md:text-left md:px-0 md:pr-4 md:max-w-3xl'>
-                I am a Frontend website developer. I specialize in creating visually appealing, user-friendly websites that are optimized for performance and search engines. Whether you need a new website or assistance with customization, I&#39;m here to deliver outstanding results. Let&#39;s collaborate and bring your online presence to life!
+                I am a front-end website developer. I specialize in creating visually appealing, user-friendly website solutions optimized for performance and search engines. Whether you need a new website assistance with customization or bug fixing I&#39;m here to deliver outstanding results. Let&#39;s collaborate, bring your ideas to life, and create something great!
             </p>
             <AnimatedButton link='https://www.fiverr.com/sreenath_kumar' text='Get In Touch' target="_blank" />
         </section>
--- app/components/ui/Modal.tsx
+++ app/components/ui/Modal.tsx
@@ -26,7 +26,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
                     animate={{ opacity: 1, height: 'auto' }}
                     exit={{ opacity: 0, height: 0 }}
                     transition={{ duration: 0.3 }}
-                    className='flex flex-col text-neutral-100 rounded-t-lg p-6 mx-auto bg-secondary md:w-3/5 md:p-10'>
+                    className='flex flex-col text-neutral-100 rounded-t-lg p-6 mx-auto bg-secondary cursor-pointer md:w-3/5 md:p-10'>
                     <svg onClick={onDismiss} xmlns="http://www.w3.org/2000/svg" className='ml-auto mb-6' width="24" height="24" viewBox="0 0 24 24"><g fill="currentColor" fillRule="evenodd" clipRule="evenodd"><path d="M5.47 5.47a.75.75 0 0 1 1.06 0l12 12a.75.75 0 1 1-1.06 1.06l-12-12a.75.75 0 0 1 0-1.06" /><path d="M18.53 5.47a.75.75 0 0 1 0 1.06l-12 12a.75.75 0 0 1-1.06-1.06l12-12a.75.75 0 0 1 1.06 0" /></g></svg>
                     {
                         children
